7296274a83e142ae7d1402d3335b04776116d016
MarHoff <m.hoffmann@adauhr.fr>
2016-11-29 17:43:06 +0100

    Globally replace 'ban' keyword by 'adresse' to better reflect API branding

--- frapi--dev.sql
+++ frapi--dev.sql
@@ -8,7 +8,7 @@ sleep $2 & wget -qO- "$1" & wait
 $BODY$
 LANGUAGE plsh VOLATILE;
 
-CREATE TYPE frapi.ban_search AS
+CREATE TYPE frapi.adresse_search AS
    (id text,
     type text,
     score numeric,
@@ -22,34 +22,34 @@ CREATE TYPE frapi.ban_search AS
     geom geometry(Point,4326)
 );
 
-CREATE OR REPLACE FUNCTION frapi.ban_search_format(raw_result jsonb)
-  RETURNS SETOF frapi.ban_search AS
+CREATE OR REPLACE FUNCTION frapi.adresse_search_format(raw_result jsonb)
+  RETURNS SETOF frapi.adresse_search AS
 $BODY$
 WITH result_array as (SELECT jsonb_array_elements_text( raw_result -> 'features')::jsonb f )
 
 SELECT
-(f #>> '{properties,id}')::text as ban_id,
-(f #>> '{properties,type}')::text as ban_type,
-(f #>> '{properties,score}')::numeric as ban_score,
-(f #>> '{properties,housenumber}')::text as ban_housenumber,
-(f #>> '{properties,name}')::text as ban_name,
-(f #>> '{properties,postcode}')::text as ban_postcode,
-(f #>> '{properties,citycode}')::text as ban_citycode,
-(f #>> '{properties,city}')::text as ban_city,
-(f #>> '{properties,context}')::text as ban_context,
-(f #>> '{properties,label}')::text as ban_label,
-ST_SetSRID(ST_GeomFromGeoJSON((f->'geometry')::text),4326)::geometry(Point,4326) as ban_geom
+(f #>> '{properties,id}')::text as adresse_id,
+(f #>> '{properties,type}')::text as adresse_type,
+(f #>> '{properties,score}')::numeric as adresse_score,
+(f #>> '{properties,housenumber}')::text as adresse_housenumber,
+(f #>> '{properties,name}')::text as adresse_name,
+(f #>> '{properties,postcode}')::text as adresse_postcode,
+(f #>> '{properties,citycode}')::text as adresse_citycode,
+(f #>> '{properties,city}')::text as adresse_city,
+(f #>> '{properties,context}')::text as adresse_context,
+(f #>> '{properties,label}')::text as adresse_label,
+ST_SetSRID(ST_GeomFromGeoJSON((f->'geometry')::text),4326)::geometry(Point,4326) as adresse_geom
 FROM result_array;
 $BODY$
 LANGUAGE sql VOLATILE
   COST 120;
 
--- Function: frapi.ban_search(text, integer, boolean, numeric, numeric, text, text, text)
+-- Function: frapi.adresse_search(text, integer, boolean, numeric, numeric, text, text, text)
 
--- DROP FUNCTION frapi.ban_search(text, integer, boolean, numeric, numeric, text, text, text);
+-- DROP FUNCTION frapi.adresse_search(text, integer, boolean, numeric, numeric, text, text, text);
 
-CREATE OR REPLACE FUNCTION frapi.ban_search(q text, "limit" integer DEFAULT 1, autocomplete boolean DEFAULT true, lon numeric DEFAULT NULL::numeric, lat numeric DEFAULT NULL::numeric, type text DEFAULT NULL::text, postcode text DEFAULT NULL::text, citycode text DEFAULT NULL::text)
-RETURNS SETOF frapi.ban_search AS
+CREATE OR REPLACE FUNCTION frapi.adresse_search(q text, "limit" integer DEFAULT 1, autocomplete boolean DEFAULT true, lon numeric DEFAULT NULL::numeric, lat numeric DEFAULT NULL::numeric, type text DEFAULT NULL::text, postcode text DEFAULT NULL::text, citycode text DEFAULT NULL::text)
+RETURNS SETOF frapi.adresse_search AS
 $BODY$
 DECLARE
 frapi_query text;
@@ -108,15 +108,15 @@ RAISE DEBUG 'type : %', (SELECT frapi_result -> 'type');
 RAISE DEBUG 'version : %', (SELECT frapi_result -> 'version');
 
 
-RETURN QUERY SELECT * FROM frapi.ban_search_format(frapi_result);
+RETURN QUERY SELECT * FROM frapi.adresse_search_format(frapi_result);
 
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 120;
 
-CREATE OR REPLACE FUNCTION frapi.ban_reverse( lon numeric, lat numeric, "limit" integer DEFAULT 1,  autocomplete boolean DEFAULT true,  type text DEFAULT NULL::text,  postcode text DEFAULT NULL::text,  citycode text DEFAULT NULL::text)
-RETURNS SETOF frapi.ban_search AS
+CREATE OR REPLACE FUNCTION frapi.adresse_reverse( lon numeric, lat numeric, "limit" integer DEFAULT 1,  autocomplete boolean DEFAULT true,  type text DEFAULT NULL::text,  postcode text DEFAULT NULL::text,  citycode text DEFAULT NULL::text)
+RETURNS SETOF frapi.adresse_search AS
 $BODY$
 DECLARE
 frapi_query text;
@@ -173,7 +173,7 @@ RAISE DEBUG 'type : %', (SELECT frapi_result -> 'type');
 RAISE DEBUG 'version : %', (SELECT frapi_result -> 'version');
 
 
-RETURN QUERY SELECT * FROM frapi.ban_search_format(frapi_result);
+RETURN QUERY SELECT * FROM frapi.adresse_search_format(frapi_result);
 
 END
 $BODY$
