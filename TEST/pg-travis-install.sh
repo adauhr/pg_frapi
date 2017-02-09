aee79761ad2216522d492e650de7ed9793524418
MarHoff <m.hoffmann@adauhr.fr>
2017-02-09 15:47:56 +0100

    adding local shell script with postgis packages

---
+++ TEST/pg-travis-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux
+
+sudo apt-get update
+
+packages="postgresql-$PGVERSION postgresql-server-dev-$PGVERSION postgresql-common postgresql-$PGVERSION-postgis-$POSTGISVERSION libtap-parser-sourcehandler-pgtap-perl"
+
+# bug: http://www.postgresql.org/message-id/20130508192711.GA9243@msgid.df7cb.de
+sudo update-alternatives --remove-all postmaster.1.gz
+
+# stop all existing instances (because of https://github.com/travis-ci/travis-cookbooks/pull/221)
+sudo service postgresql stop
+# and make sure they don't come back
+echo 'exit 0' | sudo tee /etc/init.d/postgresql
+sudo chmod a+x /etc/init.d/postgresql
+
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install $packages
+
+status=0
+sudo pg_createcluster --start $PGVERSION test -p 55435 -- -A trust
